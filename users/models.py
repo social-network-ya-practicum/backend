bbc063ab224392bc5033dbf6388d8d2d8168fd12
Andrei Bychkov <fmelodic@gmail.com>
2023-05-25 20:25:58 +0400

    Add swagger/redoc to config/urls.py

--- users/models.py
+++ users/models.py
@@ -7,21 +7,24 @@ from phonenumber_field.modelfields import PhoneNumberField
 from .managers import CustomUserManager
 
 
-
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     """Custom user model."""
 
     email = models.EmailField(_('email address'), max_length=254, unique=True)
     first_name = models.CharField(_('first_name'), max_length=50)
     last_name = models.CharField(_('last_name'), max_length=50)
-    phone_number = PhoneNumberField(_('phone_number'), max_length=50, unique=True)
+    phone_number = PhoneNumberField(
+        _('phone_number'), max_length=50, unique=True
+    )
     birthday_date = models.DateField(_('birthday_date'), max_length=50)
     password = models.CharField(_('password'), max_length=50)
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
+        help_text=_(
+            "Designates whether the user can log into this admin site."
+        ),
     )
     is_active = models.BooleanField(
         _('active'),
@@ -33,7 +36,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     )
 
     objects = CustomUserManager()
-    
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [
         'first_name', 'last_name', 'phone_number', 'birthday_date', 'password'
