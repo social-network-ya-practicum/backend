f8d8632eb10013a92eedad01f796aa163cb7d033
borrrv <vrboroday@yandex.ru>
2023-06-03 11:36:47 +0300

    Birthday

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from posts.views import PostViewSet
-
+from posts.views import BirthdayList, PostViewSet
 
 app_name = 'api'
 router_v1 = routers.DefaultRouter()
 
 router_v1.register(r'posts', PostViewSet, basename='posts')
 
+
 urlpatterns = [
     path('', include(router_v1.urls)),
+    path('birthday_list/', BirthdayList.as_view())
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,14 @@
+from datetime import datetime
+
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
+from rest_framework import generics, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from posts.models import Post
-from posts.permissions import IsAuthorAdminOrReadOnly
-from posts.serializers import PostSerializer
+#from posts.permissions import IsAuthorAdminOrReadOnly
+from posts.serializers import BirthdaySerializer, PostSerializer
+from users.models import CustomUser
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -36,3 +39,18 @@ class PostViewSet(viewsets.ModelViewSet):
         if request.method == 'DELETE':
             post.users_like.remove(request.user)
             return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BirthdayList(generics.ListAPIView):
+    serializer_class = BirthdaySerializer
+
+    def get_queryset(self):
+        today = datetime.now().date()
+        current_month = today.month
+        current_day = today.day
+        queryset = CustomUser.objects.filter(
+            birthday_date__day__gte=current_day,
+            birthday_date__day__lte=(current_day + 3),
+            birthday_date__month=current_month,
+        ).order_by('birthday_date')[:3]
+        return queryset
