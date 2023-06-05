3b3768375c757fc0980377bf6ab07658a31d6421
borrrv <vrboroday@yandex.ru>
2023-06-05 10:41:13 +0300

    Flake8

--- posts/permissions.py
+++ posts/permissions.py
@@ -9,11 +9,11 @@ class IsAuthorAdminOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-                request.user.is_authenticated and
-                (
-                        request.user.is_staff
-                        or request.user.is_superuser
-                        or obj.author == request.user
+            request.user.is_authenticated
+            and (
+                 request.user.is_staff
+                 or request.user.is_superuser
+                 or obj.author == request.user
                 )
         )
 
--- users/filters.py
+++ users/filters.py
@@ -9,30 +9,27 @@ def filter_birthday(current_day, current_month, next_month, day_one, day_two):
     Если 28 число високосного года или 27 невисоконого месяц февраль
     """
     if current_day == day_one:
-        queryset = CustomUser.objects.filter(
+        return (CustomUser.objects.filter(
             (Q(birthday_date__gte=current_day)
              & Q(birthday_date__lte=current_day + 1)
              & Q(birthday_date__month=current_month))
             | (Q(birthday_date__day=1)
                & Q(birthday_date__month=next_month))
-        ).order_by('birthday_date')[:3]
-        return queryset
+        ).order_by('birthday_date')[:3])
     """
     Если 31 число нечетного месяца или 30 число четного месяца
     Если 29 число високосного года или 28 невисокосного месяц февраль
     """
     if current_day == day_two:
-        queryset = CustomUser.objects.filter(
+        return (CustomUser.objects.filter(
             (Q(birthday_date=current_day)
              & Q(birthday_date__month=current_month))
             | ((Q(birthday_date__day=1)
                | Q(birthday_date__day=2))
                & Q(birthday_date__month=next_month))
-        ).order_by('birthday_date')[:3]
-        return queryset
-    queryset = CustomUser.objects.filter(
+        ).order_by('birthday_date')[:3])
+    return (CustomUser.objects.filter(
         birthday_date__day__gte=current_day,
         birthday_date__day__lt=(current_day + 3),
         birthday_date__month=current_month
-    ).order_by('birthday_date')[:3]
-    return queryset
+    ).order_by('birthday_date')[:3])
