31a649378b0a02f13e3d9317d8253793538f39cb
Дмитрий Спиряев <imwisagist@yandex.ru>
2023-06-09 03:11:58 +0300

    Tokens was unregistered from admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from rest_framework.authtoken.models import TokenProxy
 from .models import CustomUser
 
 
@@ -37,3 +37,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.unregister(TokenProxy)
