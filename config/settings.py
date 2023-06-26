6e141e2335b3954c0b51a63eacfba46962e5d2e8
Дмитрий Спиряев <imwisagist@yandex.ru>
2023-06-26 11:45:59 +0300

    CSRF Fixed and Enabled

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_django_key'),
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
 
 ROOT_URLCONF = 'config.urls'
 
@@ -18,7 +18,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 # locale.setlocale(locale.LC_ALL, "ru_RU.utf8")
 
