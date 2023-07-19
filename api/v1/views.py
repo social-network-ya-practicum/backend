37fce4aefaa64b685d402884cd7722d10f007d67
Ivan Konyshkin <konyshkinn2001@gmail.com>
2023-07-19 14:41:49 +0500

    fixed response delete like on comments

--- api/v1/views.py
+++ api/v1/views.py
@@ -119,7 +119,10 @@ class CommentsViewSet(ModelViewSet):
     def delete_like(self, request, *args, **kwargs):
         comment = get_object_or_404(Comment, id=self.kwargs['pk'])
         comment.like.remove(request.user)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        serializer = CommentSerializer(comment)
+        return Response(
+            data=serializer.data, status=status.HTTP_200_OK
+        )
 
 
 class ChangePasswordView(CreateAPIView):
